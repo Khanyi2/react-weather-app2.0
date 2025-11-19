153c063db647c408fee816b668acdc9f7ae92d6e
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-19 19:44:28 +0200

    added a footer on app.js.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Khanyi {" "}
+      <a href='https://github.com/Khanyi2/react-weather-app2.0' target='_blank'> and open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
