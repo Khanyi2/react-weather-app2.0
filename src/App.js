5573df5510c20e59a0e733e3c96ff313ae625af5
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-19 21:42:32 +0200

    HTML structure of the application.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Khanyi {" "}
       <a href='https://github.com/Khanyi2/react-weather-app2.0' target='_blank' rel="noreferrer"> and open-sourced on GitHub</a>
