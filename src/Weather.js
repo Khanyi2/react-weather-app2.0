2cb057db8a6543f881572074913eba88fd593a10
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-20 09:27:10 +0200

    added api key and url.

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,10 @@
 import React from "react";
+import axios from "axios";
 import './Weather.css';
 
 export default function Weather() {
+    const apiKey = "d0o3t764a6d83cea9c0bfd4fda2bd33e";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}&units=metric`;
     return (
         <div className="Weather">
             <form>
