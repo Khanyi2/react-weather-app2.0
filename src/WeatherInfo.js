66b5460d283ad33a2783664b9fadb11636d642b3
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-22 16:15:45 +0200

    updated the components

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,41 +1,37 @@
+import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    const data = props.data;
-
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalized">{data.description}</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
-
       <div className="row mt-3">
-        {/*ICON + TEMPERATURE*/}
-        <div className="col-6 d-flex align-items-center">
-            <WeatherIcon code={data.icon} size={64} />
-            <div className="ms-3">
-                 <WeatherTemperature celsius={data.temperature} />
-            </div>     
-        </div>
-         
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
 
-        {/*DETAILS*/}
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 10%</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
     </div>
-    
   );
 }
