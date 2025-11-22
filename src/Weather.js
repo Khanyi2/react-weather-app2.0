8a7644265ff211ea4e8b7bdb22848c252956ec6a
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-22 16:02:34 +0200

    fixed Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import './Weather.css';
 import WeatherInfo from "./WeatherInfo";
@@ -38,30 +38,40 @@ export default function Weather(props) {
         setCity(event.target.value);
     }
 
-    useEffect(() => {
-        search();
-    }, []);
+    // useEffect(() => {
+    //     search();
+    // }, []);
 
 
-    if (weatherData.ready){
-        return <div>Loading ...</div>;
-    }
-    
-        return(
-        <div className="Weather">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control"  autoFocus="on"
-                onChange={handleCityChange}/>
-                </div>
-                <div className="col-3">
-                <input type="submit" value="Search" className="btn-primary" />
-                </div>
-                </div>
-            </form>
-        <WeatherInfo data={weatherData}/>    
-        </div>
-        );
+    if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
     
 }
