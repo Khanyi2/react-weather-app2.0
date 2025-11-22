af56d917469a5a4862449d41783ab99540d5f17f
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-11-22 15:03:50 +0200

    created a method to convert fahreneit and celsius.

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,18 +3,36 @@ import React, {useState} from "react";
 
 export default function WeatherTemperature(props) {
 let [unit, setUnit] = useState("celsius");
+
+function convertToCelsius(event){
+    event.preventDefault();
+    setUnit("celsius");
+}
+
+function convertToFahrenheit(event){
+    event.preventDefault();
+    setUnit("fahrenheit");
+}
 if (unit === "celsius") {
     return (
         <div className="WeatherTemperature">
         <span className="temperature">
               {Math.round(props.data.temperature)}
             </span>
-            <span className="unit">°C</span>
-
+            <span className="unit">°C | <a href="/" onClick={convertToFahrenheit}>°F</a></span>
         </div>
     );
 } else {
-    return "F";
+    return(
+    <div className="WeatherTemperature">
+        <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">
+                <a href="/" onClick={convertToCelsius}>°C </a>
+                {" "}| °F</span>
+        </div>
+    );
 }
 }
 
